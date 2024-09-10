5de9dde873bdfa6a93d3ca6cca6e9bf1cf13186d
Rui Barbosa <barduinor@gmail.com>
2024-09-10 15:53:57 -0400

    inteligence extract init and sample docs

---
+++ workshops/intelligence_extract/create_samples.py
@@ -0,0 +1,18 @@
+"""Uploads or Creates samples for exercises."""
+
+import logging
+
+from box_sdk_gen.client import BoxClient as Client
+
+from utils.box_utils import create_box_folder, folder_upload
+
+logging.getLogger(__name__)
+
+
+def create_samples(client: Client):
+    """Uploads sample content to Box."""
+    wks_folder = create_box_folder(client, "workshops", client.folders.get_folder_by_id("0"))
+
+    docs_folder = create_box_folder(client, "intelligence", wks_folder)
+
+    folder_upload(client, docs_folder, "workshops/intelligence_extract/content_samples/")

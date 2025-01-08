b571c733c7004c6138bc6339e13b01cf2c7dc67c
Rui Barbosa <barduinor@gmail.com>
2025-01-08 15:07:08 -0500

    clean up

--- workshops/doc_gen/doc_gen_sln.py
+++ workshops/doc_gen/doc_gen_sln.py
@@ -1,6 +1,6 @@
 import logging
 import random
-from datetime import date
+from datetime import date, datetime
 from time import sleep
 from typing import List
 
@@ -25,7 +25,7 @@ LEASE_TEMPLATE_ID = "1744637428174"
 LEASES_FOLDER_ID = "301836779172"
 
 
-def mark_file_as_doc_gen_template(client: BoxClient, file_id: str) -> DocgenTemplate:
+def set_file_as_template(client: BoxClient, file_id: str) -> DocgenTemplate:
     """Mark a file as a DocGen template"""
     # check if file exists and it is accessible
     file = client.files.get_file_by_id(file_id)
@@ -73,7 +73,7 @@ def generate_new_data(name: str, email: str, start_date: date) -> DocgenDocument
             "Description": "Two private and spacious bedrooms, each equipped with a temperature-regulating system, offering breathtaking views of the lunar landscape through reinforced transparent panels. Bedrooms are fitted with built-in storage for personal items and lunar suits.",
             "StartDate": start_date.isoformat(),
             "EndDate": end_date.isoformat(),
-            "Rent": "5535",
+            "Rent": f"${5535:,.2f}",
         },
     )
 
@@ -89,7 +89,7 @@ def main():
     print()
 
     # Set the MS Word file as a doc gen template
-    template = mark_file_as_doc_gen_template(client, LEASE_TEMPLATE_ID)
+    template = set_file_as_template(client, LEASE_TEMPLATE_ID)
     print(f"Template created: {template.to_dict()}")
 
     # List template tags
@@ -116,15 +116,53 @@ def main():
     batch = generate_new_document(client, template.file.id, LEASES_FOLDER_ID, docs_data)
     print(f"\nNew batch created: {batch.to_dict()}")
 
-    # get jobs in batch
+    # Get jobs in batch
+    print("\nJobs in batch:")
     jobs = client.doc_gen.get_docgen_batch_job_by_id(batch.id)
     for job in jobs.entries:
-        print(f"Job {job.id} - {job.status.name}")
-    sleep(10)
-    print("\nAfter 10 seconds:")
-    jobs = client.doc_gen.get_docgen_batch_job_by_id(batch.id)
+        print(f"  - Job {job.id} - {job.status.name}")
+
+    # Lis jobs in batch
+    print("\nJob details:")
     for job in jobs.entries:
-        print(f"Job {job.id} - {job.status.name}")
+        job_details = client.doc_gen.get_docgen_job_by_id(job.id)
+        print(f"  - {job_details.to_dict()}\n")
+        sleep(3)
+
+    # List all jobs for user
+    user_jobs = client.doc_gen.get_docgen_jobs(limit=50)
+    print("\nAll jobs for current user:")
+    for job in user_jobs.entries:
+        print(f"  - Job {job.id} {datetime.fromtimestamp(int(job.created_at)).isoformat()} {job.status.name}")
+
+    # List all jobs by template
+    template_jobs = client.doc_gen_template.get_docgen_template_job_by_id(template.file.id)
+    print("\nAll jobs for template:")
+    for job in template_jobs.entries:
+        print(f"  - Job {job.id} {datetime.fromtimestamp(int(job.created_at)).isoformat()} {job.status.name}")
+
+    # Remove the template
+    client.doc_gen_template.delete_docgen_template_by_id(template.file.id)
+
+    # List all templates
+    templates = client.doc_gen_template.get_docgen_templates()
+    print("\nAll templates:")
+
+    if templates.entries:
+        for template in templates.entries:
+            print(f"  - {template.to_dict()}")
+    else:
+        print("  - No templates found")
+
+    # print(
+    #     "\nAll templates:",
+    #     *(
+    #         [f"  - {template.to_dict()}" for template in templates.entries]
+    #         if templates.entries
+    #         else ["  - No templates found"]
+    #     ),
+    #     sep="\n",
+    # )
 
 
 if __name__ == "__main__":
